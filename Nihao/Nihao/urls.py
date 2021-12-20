229d8097f0db549404ea7175d4e6381874a347fa
shunna87 <anush_av@instigatemobile.com>
2021-12-20 18:28:37 +0400

    erkushabti

--- Nihao/Nihao/urls.py
+++ Nihao/Nihao/urls.py
@@ -1,4 +1,4 @@
-"""Shunna URL Configuration
+"""Nihao URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('Diary_china.urls')),
+    path('', include('blog.urls')),
 
 ]
+
--- Nihao/Nihao/wsgi.py
+++ Nihao/Nihao/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Shunna project.
+WSGI config for Nihao project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Shunna.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Nihao.settings')
 
 application = get_wsgi_application()
